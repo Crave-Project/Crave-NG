40170a855b18709298a61e0a27808d165edcc116
CooleRRSA <cooler.int13h@gmail.com>
2018-10-05 15:53:19 +0300

    Dependencies building fix

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -4,13 +4,13 @@ native_packages := native_ccache
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_x86_64_linux_packages:=qt56 expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 qt_arm_linux_packages:=$(qt_x86_64_linux_packages)
 qt_aarch64_linux_packages:=$(qt_x86_64_linux_packages)
 
-qt_darwin_packages=qt
-qt_mingw32_packages=qt
+qt_darwin_packages=qt59
+qt_mingw32_packages=qt56
 
 wallet_packages=bdb
 
